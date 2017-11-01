825b905d00324d37dc89ef35203b4123bfe1fb1c
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-11-02 00:59:00 +0300

    add for cycle for messages

--- naive_bayes_classifier/main.py
+++ naive_bayes_classifier/main.py
@@ -5,8 +5,10 @@ import reader
 from model.message import MessageType
 
 cv = ml.create_cv_from_blocks(reader.read_blocks("pu1"))
-__dict = ml.create_dict(cv[0]["test"], count_twice=False)
+__dict = ml.create_dict(cv[0]["train"], count_twice=False)
 i = 4
 
-print sd.get_message_spam_probability(__dict, cv[0]["test"][i])
-print cv[0]["test"][i].type
+for message in cv[0]["test"]:
+    print "--------------"
+    print sd.get_message_spam_probability(__dict, message)
+    print message.type
