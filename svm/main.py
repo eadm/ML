990718a11b268267eb944f4013a63ed64f48feac
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-11-16 01:43:38 +0300

    fix w count

--- svm/main.py
+++ svm/main.py
@@ -13,22 +13,26 @@ points, classes = reader.read_data("chips.txt")
 folds = ml.folds(points, classes, FOLDS, shuffle=True)
 
 # print folds
+C = 1.1
+EPS = 0.0000001
 
-slv = opt.solve_sp(folds[0]["train_p"], folds[0]["train_c"], lambda x, y: kernels.gaussian(x, y, 0.5), 1.1)
+slv = opt.solve_sp(folds[0]["train_p"], folds[0]["train_c"], lambda x, y: kernels.gaussian(x, y, 0.5), C)
 slv = slv.x
 print slv
-
 # l = gradient(folds[0]["train_p"], folds[0]["train_c"], 0.1, 0.0001, lambda x, y: kernels.gaussian(x, y, 1.0))
-w = np.zeros(folds[0]["train_p"][0].size)
-for i in range(slv.size):
-    w += slv[i] * folds[0]["train_c"][i] * folds[0]["train_p"][i]
-# # w = np.sum(l * folds[0]["train_c"] * folds[0]["train_p"])
-# print l
-print w
-#
+
+
+def classify(p, ls, train_p, train_c):
+    __sum = 0.0
+    for i in range(ls.size):
+        if ls[i] != C and ls[i] > EPS:
+            __sum += np.dot(train_p[i], p) * ls[i] * train_c[i]
+    return int(np.sign(__sum))
+
+
 ans_c = []
 for p in folds[0]["test_p"]:
-    ans_c.append(int(np.sign(np.dot(w, p))))
+    ans_c.append(classify(p, slv, folds[0]["train_p"], folds[0]["train_c"]))
 
 print ans_c
 print ml.contingency(folds[0]["test_c"], ans_c)
