370ff23cc380d17b6b3bdda9c50e5f85338f8d89
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-11-16 00:41:52 +0300

    fix w count

--- svm/main.py
+++ svm/main.py
@@ -15,14 +15,13 @@ folds = ml.folds(points, classes, FOLDS, shuffle=True)
 # print folds
 
 slv = opt.solve_sp(folds[0]["train_p"], folds[0]["train_c"], lambda x, y: kernels.gaussian(x, y, 0.5), 1.1)
-print slv
-slv = np.array(slv)
+slv = slv.x
 print slv
 
 # l = gradient(folds[0]["train_p"], folds[0]["train_c"], 0.1, 0.0001, lambda x, y: kernels.gaussian(x, y, 1.0))
 w = np.zeros(folds[0]["train_p"][0].size)
 for i in range(slv.size):
-    w += slv[i][0] * folds[0]["train_c"][i] * folds[0]["train_p"][i]
+    w += slv[i] * folds[0]["train_c"][i] * folds[0]["train_p"][i]
 # # w = np.sum(l * folds[0]["train_c"] * folds[0]["train_p"])
 # print l
 print w
