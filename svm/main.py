7012595e9f2c12176fb95df9868a417f4da59cde
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-11-16 03:18:24 +0300

    ready to print

--- svm/main.py
+++ svm/main.py
@@ -4,6 +4,8 @@ import kernels
 import numpy as np
 import opt
 
+# f = open("out", mode='w+')
+
 FOLDS = 10
 
 points, classes = reader.read_data("chips.txt")
@@ -37,6 +39,7 @@ def check_fold(fold, c, sigma):
     ans_c = []
     slv = opt.solve_sp(fold["train_p"], fold["train_c"], lambda x, y: kernels.gaussian(x, y, sigma), c).x
 
+    print slv
     w = get_w(slv, fold["train_p"], fold["train_c"])
     b = get_b(w, fold["train_p"], fold["train_c"])
 
@@ -46,20 +49,32 @@ def check_fold(fold, c, sigma):
 
 
 def check(_folds, c, sigma):
+    print "--------------------"
+    print "C:"
+    print c
+    print "Sigma:"
+    print sigma
     ans_c = []
     ans_p = []
     for fold in _folds:
         ans_c.extend(check_fold(fold, c, sigma))
         ans_p.extend(fold["train_c"])
 
-    return ml.contingency(ans_p, ans_c)
+    ctg = ml.contingency(ans_p, ans_c)
+    print ctg
+    return ctg
 
 
-def classify(p, ls, train_p, train_c, c,  b):
+def classify(p, ls, train_p, train_c, c, b):
     __sum = 0.0
     for i in range(ls.size):
         if EPS < ls[i] < c - EPS:
+            print "----"
             print ls[i]
+            print train_p[i]
+            print train_c[i]
             __sum += np.dot(train_p[i], p) * ls[i] * train_c[i]
     return int(np.sign(__sum))
 
+
+check_fold(folds[0], 2.5, 1.2)
