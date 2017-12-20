f9d9f98e22a3f32e23866310168d0cc4a3dc6cfd
Ruslan Davletshin <smartmorefeed@gmail.com>
2017-12-21 02:18:03 +0300

    train

--- cnn/main.py
+++ cnn/main.py
@@ -1,9 +1,38 @@
 # import mnist
 from nn import NN
+import numpy as np
 
 
 nn = NN()
-for i in range(28):
-    nn.add_empty_layer(768, 768)
+# for i in range(28):
+#     nn.add_empty_layer(768, 768)
 
-NN.from_file('dmp.txt')
+# NN.from_file('dmp.txt')
+
+nn.add_empty_layer(2, 4)
+nn.add_empty_layer(4, 2)
+
+# print nn.layers
+print nn.train(np.array([0.5, 0.5]), np.array([1, 0]))
+print nn.train(np.array([-0.5, -0.5]), np.array([1, 0]))
+
+# print nn.layers
+
+for i in range(100000):
+    rnd = np.random.rand(2)
+    nn.train(rnd, np.array([1, 0]))
+    rnd[0] *= -1.
+    nn.train(rnd, np.array([0, 1]))
+    rnd[1] *= -1.
+    nn.train(rnd, np.array([1, 0]))
+    rnd[0] *= -1.
+    q = nn.train(rnd, np.array([0, 1]))
+    if i % 1000 == 0:
+        print i, q
+
+nn.to_file('dmp.txt')
+
+print nn.predict(np.array([0.5, 0.5]))
+print nn.predict(np.array([-0.5, -0.5]))
+
+# nn.to_file('')
--- cnn/nn.py
+++ cnn/nn.py
@@ -1,10 +1,13 @@
 import numpy as np
+from math import exp
 
 
 class NN:
-
     def __init__(self):
         self.layers = []
+        self.sigma = np.vectorize(lambda x: 1. / (1. + exp(-x)))
+        self.sigma1 = lambda x: self.sigma(x) * (1. - self.sigma(x))
+        self.nu = 3.
 
     def add_layer(self, matrix):
         self.layers.append(matrix)
@@ -23,6 +26,39 @@ class NN:
             f.write('|')
             if i + 1 != len(self.layers):
                 f.write('\n')
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = self.sigma(np.dot(x, layer))
+        return x
+
+    def train(self, x, y):
+        y1 = x
+        ys1 = [x]
+        for layer in self.layers:
+            y1 = self.sigma(np.dot(y1, layer))
+            ys1.append(y1)
+
+        es = [y1 - self.sigma(y)]
+        q = np.sum(es[-1] ** 2)
+
+        for i in reversed(range(len(self.layers))):
+            layer = self.layers[i]
+            s1 = self.sigma1(ys1[i + 1])
+            eps = np.dot(layer, es[-1] * s1)
+            es.append(eps)
+        es = list(reversed(es))
+
+        for k in range(len(self.layers)):
+            layer = self.layers[k]
+
+            y1 = ys1[k]
+            s1 = self.sigma1(y1)
+            for i in range(len(layer)):
+                for j in range(len(layer[i])):
+                    layer[i][j] = layer[i][j] - self.nu * (es[k][i] * s1[i] * ys1[k + 1][j])
+
+        return q
 
     @staticmethod
     def from_file(path):
